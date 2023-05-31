373d9c78ce1cbc73758f76f0fa348715da09e344
JHY <871853191@qq.com>
2023-05-31 16:38:30 +0800

    客户端：跨平台版本，可以在Windows、linux、macos中运行

--- EasyTcpClient/client.cpp
+++ EasyTcpClient/client.cpp
@@ -1,9 +1,21 @@
 #define WIN32_LEAN_AND_MEAN
 //#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#ifdef _WIN32
 #include<Windows.h>
 #include<WinSock2.h>
+#else
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<string.h>
+
+#define SOCKET int
+#define INVALID_SOCKET  (SOCKET)(~0)
+#define SOCKET_ERROR            (-1)
+#endif
 #include<iostream>
 #include<stdio.h>
+#include<thread>
 #pragma comment(lib,"ws2_32.lib") //仅支持windows系统，引入静态链接库
 
 
@@ -111,7 +123,7 @@ int processor(SOCKET _cSock)
 	{
 		recv(_cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
 		NewUserJoin* userjoin = (NewUserJoin*)szRecv;
-		printf("收到服务端返回消息:CMD_NEW_USER_JOIN,客户端<SOCKET = %d>加入， 数据长度:%d\n", userjoin->sock, userjoin->dataLength);
+		printf("收到服务端返回消息:CMD_NEW_USER_JOIN,客户端<SOCKET = %d>加入， 数据长度:%d\n", (int)userjoin->sock, userjoin->dataLength);
 	}
 	break;
 	default:
@@ -121,14 +133,45 @@ int processor(SOCKET _cSock)
 	}
 	break;
 	}
+	return 0;
+}
 
+bool g_bRun = true;
+//线程处理函数
+void cmdThread(SOCKET sock)
+{
+	//利用线程进行命令输入
+	while (true)
+	{
+		char cmdBuf[256] = {};
+		scanf("%s", cmdBuf);
+		if (0 == strcmp(cmdBuf, "exit"))
+		{
+			g_bRun = false;
+			printf("退出cmdThread线程.\n");
+			break;
+		}
+		else if (0 == strcmp(cmdBuf, "login"))
+		{
+			Login login;
+			strcpy(login.userName, "JHY");
+			strcpy(login.PassWord, "666");
+			send(sock, (const char*)&login, sizeof(Login), 0);
+		}
+		else
+		{
+			printf("不支持的命令，请重新输入.\n");
+		}
+	}
 }
 int main()
 {
+#ifdef _WIN32
 	//启动Windows Socket 2.x 环境
 	WORD ver = MAKEWORD(2, 2);//WORD版本号
 	WSADATA dat;
 	WSAStartup(ver, &dat);//启动win Socket
+#endif
 	//用四步实现简易的TCP客户端
 	//1、建立一个socket
 	SOCKET _sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -144,7 +187,11 @@ int main()
 	sockaddr_in _sin = {};
 	_sin.sin_family = AF_INET;
 	_sin.sin_port = htons(4567);
+#ifdef _WIN32
 	_sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+#else
+	_sin.sin_addr.s_addr = inet_addr("172.23.80.117");
+#endif
 	int ret = connect(_sock, (sockaddr*)&_sin, sizeof(sockaddr_in));
 	if (SOCKET_ERROR == ret)
 	{
@@ -154,14 +201,17 @@ int main()
 	{
 		printf("TRUE,连接Socket成功.\n");
 	}
+	//启动线程
+	std::thread t1(cmdThread, _sock);
+	t1.detach();
 
-	while (true)
+	while (g_bRun)
 	{
 		fd_set fdReads;
 		FD_ZERO(&fdReads);
 		FD_SET(_sock, &fdReads);
-		timeval t = { 1,0 };
-		int ret = select(_sock, &fdReads, 0, 0, &t);
+		timeval t = { 1, 0 };
+		int ret = select(_sock + 1, &fdReads, 0, 0, &t);
 		if (ret < 0)
 		{
 			printf("select任务结束1..\n");
@@ -177,21 +227,20 @@ int main()
 				break;
 			}
 		}
-	
-		printf("客户端处于空闲时间，处理其他业务..\n");
-		//主动向服务器发送登录等消息
-		Login login;
-		strcpy(login.userName,"JHY");
-		strcpy(login.PassWord, "666");
-		send(_sock, (const char*)&login, sizeof(Login),0);
-		Sleep(1000);
+
+		//printf("客户端处于空闲时间，处理其他业务..\n");
+
 
 	}
-		//7、关闭套接字closesocket
-		closesocket(_sock);
-		//清楚Windows Socket环境
-		WSACleanup();//关闭win Socket
-		getchar();//防止程序一闪而过
-		return 0;
+	//7、关闭套接字closesocket
+#ifdef _WIN32
+	closesocket(_sock);
+	//清楚Windows Socket环境
+	WSACleanup();//关闭win Socket
+#else
+	close(_sock);
+#endif
+	getchar();//防止程序一闪而过
+	return 0;
 
 }
